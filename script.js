61296febf31c72327816465e835a83d0d0c0038a
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-13 17:14:10 -0500

    Valida confirmacion de contraseña

---
+++ script.js
@@ -0,0 +1,8 @@
+const password=document.querySelector("#password");
+const conPassword=document.querySelector("#conPassword");
+
+conPassword.addEventListener("input",()=>{
+    if(password.value!=conPassword.value){
+
+    }
+})

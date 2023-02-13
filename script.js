9f60a2cd5d2ef1641145202657c7af01ef12b4f7
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-13 17:52:36 -0500

    Añade codigo para cambiar color en confirmar contra

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const conPassword=document.querySelector("#conPassword");
 
 conPassword.addEventListener("input",()=>{
     if(password.value!=conPassword.value){
-
+        password.classList.add("error");
+        conPassword.classList.add("error");
+    }
+    else{
+        if(password.classList){
+            password.classList.remove("error");
+            conPassword.classList.remove("error");
+        }
     }
 })

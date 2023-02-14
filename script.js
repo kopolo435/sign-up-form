7802513c09d570be9842142e582aeb0e6dadaa59
Kopolo435 <121562096+kopolo435@users.noreply.github.com>
2023-02-13 19:48:03 -0500

    Añade codigo que muestra mensaje de error
    Este codigo tambien se encarga de eliminar el mensaje de error cuando ya
    no es necesario

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const password=document.querySelector("#password");
 const conPassword=document.querySelector("#conPassword");
+const errorMsg=document.querySelector("#password+ span.errorMsg");
 
 conPassword.addEventListener("input",()=>{
     if(password.value!=conPassword.value){
-        password.classList.add("error");
-        conPassword.classList.add("error");
-    }
-    else{
-        if(password.classList){
+        if(!(conPassword.value)){
             password.classList.remove("error");
             conPassword.classList.remove("error");
+            errorMsg.textContent="";
+        }else{
+            password.classList.add("error");
+            conPassword.classList.add("error");
+            errorMsg.textContent="* Passwords do not match";
+        }
+
+    }
+    else{
+        if(password.classList || !(conPassword.value)){
+            password.classList.remove("error");
+            conPassword.classList.remove("error");
+            errorMsg.textContent="";
         }
     }
 })
